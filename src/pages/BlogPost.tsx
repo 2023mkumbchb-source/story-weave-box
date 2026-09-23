f461a5deafa3b9b99fe149d054f11fc3a1d1ecef
2023mkumbchb-source <2023mkumbchb@gmail.com>
2026-09-23 09:59:10 +0300

    Display Vimeo videos on study articles

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -6,7 +6,7 @@ import {
 } from "lucide-react";
 import ShareButtons from "@/components/ShareButtons";
 import ArticleComments from "@/components/ArticleComments";
-import { Countdown, HtmlEmbed, PasswordGate, ContentToc, ReadingTimeBadge } from "@/components/ContentExtras";
+import { Countdown, HtmlEmbed, VimeoEmbed, PasswordGate, ContentToc, ReadingTimeBadge } from "@/components/ContentExtras";
 import { motion, AnimatePresence } from "framer-motion";
 import { getArticleBySlugOrId, getPublishedArticleSummaries, getRelatedContent, getCategoryDisplayName, getYearFromCategory, buildBlogPath, buildMcqPath, buildFlashcardPath, type Article } from "@/lib/store";
 import { extractFirstImageFromContent, SITE_URL, stripRichText, updateMetaTags } from "@/lib/seo";
@@ -1819,6 +1819,7 @@ export default function BlogPost() {
             <SourceAttribution article={article} />
 
             <HtmlEmbed data={(article as any).html_embed} position="top" />
+            <VimeoEmbed url={(article as any).html_embed?.vimeo_url} />
 
             {(article as any).toc_enabled && <ContentToc content={article.content} />}
 
